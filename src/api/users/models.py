5595d62e3f26be1be9d091815cfae94de2dca7c6
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-06-27 14:33:29 -0400

    format table class

--- src/api/users/models.py
+++ src/api/users/models.py
@@ -4,9 +4,18 @@ from src import db
 class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key= True)
-    name = db.Column(db.String())
-    location = db.Column(db.String())
+    id = db.Column(
+        db.Integer, 
+        primary_key= True)
+    
+    name = db.Column(
+        db.String(),
+        nullable=False
+        )
+    
+    location = db.Column(
+        db.String()
+        )
 
     def __init__(self, name, location):
         self.name = name
