52a9c729480d6c76b81923ceab4c3c4a30f30dc9
Emily Walker <ewalker415@gmail.com>
2024-06-27 15:29:44 -0700

    add resource for user with get view and post view for user list

--- src/api/users/views.py
+++ src/api/users/views.py
@@ -1,5 +1,6 @@
+from urllib import request
 from flask_restx import Namespace, Resource, fields
-from .crud import read_users
+from .crud import read_users, read_user, create_user
 users_namespace = Namespace('users')
 
 user_model = users_namespace.model('User',{
@@ -11,10 +12,42 @@ user_model = users_namespace.model('User',{
 class UserList(Resource):
     @users_namespace.marshal_list_with(user_model)
     def get(self):
-        try: 
+        try:
             users = read_users()
             return users, 200
         except ValueError as e:
             users_namespace.abort(500, str(e))
 
+    @users_namespace.expect(user_model, validate=True)
+    @users_namespace.marshal_with(user_model, code=201)
+    def post(self):
+        try:
+            data = request.get_json()
+            name = data['name']
+            location = data['location']
+            user = create_user(name=name, location=location)
+            return user, 201
+        except ValueError as e:
+            users_namespace.abort(400, str(e))
+        except KeyError as e:
+            users_namespace.abort(400, f"Missing field: {e}")
+        except Exception as e:
+            users_namespace.abort(500, str(e))
+
 users_namespace.add_resource(UserList, '/')
+
+class UserResource(Resource):
+    @users_namespace.marshal_with(user_model)
+    def get(self, id):
+        try:
+            user = read_user(id)
+            if user:
+                return user, 200
+            else:
+                users_namespace.abort(404, f"User id:{id} not found")
+        except ValueError as e:
+            users_namespace.abort(500, str(e))
+
+users_namespace.add_resource(UserResource, '/<int:id>')
+
+
