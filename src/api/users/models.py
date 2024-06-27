1449ac0f73377648156a42ec90c1ddddc6a75193
Emily Walker <ewalker415@gmail.com>
2024-06-27 10:47:36 -0700

    users model

---
+++ src/api/users/models.py
@@ -0,0 +1,20 @@
+from src import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String())
+    location = db.Column(db.String())
+
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
+
+
+
+
+
+
