941c8b73000b69c7ee7e64d9518a400e235c79e9
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-07-04 10:15:51 -0400

    add Answer and Comment models

--- src/api/questions/models.py
+++ src/api/questions/models.py
@@ -3,34 +3,108 @@ from datetime import datetime
 
 
 class Question(db.Model):
-    __tablename__= "questions"
+    __tablename__ = "questions"
 
     id = db.Column(
         db.Integer,
-        primary_key = True
+        primary_key=True
     )
 
     user_id = db.Column(
         db.Integer,
         db.ForeignKey('users.id'),
-        nullable = False
+        nullable=False
     )
 
     content = db.Column(
         db.Text,
-        nullable = False
+        nullable=False
     )
 
     created_at = db.Column(
         db.DateTime,
-        default = db.func.current_timestamp(),
-        nullable = False
+        default=db.func.current_timestamp(),
+        nullable=False
     )
 
-    def __init__(self, user_id, content, created_at = None):
+    def __init__(self, user_id, content, created_at=None):
         self.user_id = user_id
         self.content = content
         self.created_at = created_at if created_at else datetime.now()
 
 
+class Answer(db.Model):
+    __tablename__ = "answers"
 
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        nullable=False)
+
+    question_id = db.Column(
+        db.Integer,
+        db.ForeignKey('questions.id'),
+        nullable=False)
+
+    content = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=db.func.current_timestamp(),
+    )
+
+    question = db.relationship(
+        'Question',
+        backref=db.backref('answers', lazy=True)
+    )
+
+    def __init__(self, user_id, question_id, content, created_at=None):
+        self.user_id = user_id
+        self.question_id = question_id
+        self.content = content
+        self.created_at = created_at if created_at else datetime.now()
+
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        nullable=False
+    )
+
+    answer_id = db.Column(
+        db.Integer,
+        db.ForeignKey('answers.id'),
+        nullable=False
+    )
+
+    content = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=db.func.current_timestamp(),
+    )
+
+    def __init__(self, user_id, answer_id, content, created_at=None):
+        self.user_id = user_id
+        self.answer_id = answer_id
+        self.content = content
+        self.created_at = created_at if created_at else datetime.now()
