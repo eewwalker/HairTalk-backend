f468078bafbb644d488a6e9020ba50f324de62b3
Emily Walker <ewalker415@gmail.com>
2024-07-10 16:25:52 -0700

    add auth views

---
+++ src/api/auth/views.py
@@ -0,0 +1,69 @@
+from flask import request
+from flask_restx import Resource, Namespace
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from src.api.users.models import User
+from ..users.crud import read_users, create_user
+from ..users.views import user_model
+
+
+bcrypt = Bcrypt()
+
+auth_namespace = Namespace('auth')
+
+class Register(Resource):
+    @auth_namespace.expect(user_model, validate=True)
+    def post(self):
+        try:
+            data = request.get_json()
+            username = data.get('username')
+            password = data.get('password')
+            location = data.get('location')
+
+            users = read_users()
+            if any(user.username == username for user in users):
+                return {'message': 'User already exists'}, 400
+
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            create_user(username=username, password=hashed_password, location=location)
+            return {'message': f'User:{username} registered'}, 201
+
+        except ValueError as e:
+            return {'message': str(e)}, 500
+        except Exception as e:
+            return {'message': f"An unexpected error occurred: {e}"}, 500
+
+
+class Login(Resource):
+    @auth_namespace.expect(user_model, validate=True)
+    def post(self):
+        try:
+            data = request.get_json()
+            username = data.get('username')
+            password = data.get('password')
+
+            user = User.query.filter_by(username=username).first()
+            if not user or not bcrypt.check_password_hash(user.password, password):
+                return {'message': 'Invalid credentials'}, 401
+
+            access_token = create_access_token(identity=username)
+            return {'access_token': access_token}, 200
+
+        except ValueError as e:
+            return {'message': str(e)}, 500
+        except Exception as e:
+            return {'message': f"An unexpected error occurred: {e}"}, 500
+
+
+class Protected(Resource):
+    @jwt_required()
+    def get(self):
+        try:
+            current_user = get_jwt_identity()
+            return {'logged_in_as': current_user}, 200
+        except Exception as e:
+            return {'message': f'An unexpected error occurred: {e}'}, 500
+
+auth_namespace.add_resource(Register, '/register')
+auth_namespace.add_resource(Login, '/login')
+auth_namespace.add_resource(Protected, '/protected')
